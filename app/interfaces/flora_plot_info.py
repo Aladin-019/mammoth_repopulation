af3c8209623cf552d8e5b84688513cd0b135c092
Aladin <aaladino581@gmail.com>
2025-08-06 12:03:26 -0600

    added more methods to plot information and flora plot information interfaces

--- app/interfaces/flora_plot_info.py
+++ app/interfaces/flora_plot_info.py
@@ -1,17 +1,29 @@
-# extended plot information interface for flora only
+# Extended plot information interface for Flora and its subclasses only
 
 from abc import abstractmethod
 from app.interfaces.plot_info import PlotInformation
 
 class FloraPlotInformation(PlotInformation):
     @abstractmethod
-    def get_rainfall(self) -> float:
+    def get_current_rainfall(self, day: int) -> float:
         pass
 
     @abstractmethod
-    def get_snowfall(self) -> float:
+    def get_current_snowfall(self, day: int) -> float:
         pass
 
     @abstractmethod
-    def get_uv_index(self) -> float:
+    def get_current_uv(self, day: int) -> float:
+        pass
+
+    @abstractmethod
+    def get_current_soil_temp(self, day: int) -> float:
+        pass
+
+    @abstractmethod
+    def get_current_melt_water_mass(self, day: int) -> float:
+        pass
+
+    @abstractmethod
+    def get_plot_area(self) -> float:
         pass
--- app/interfaces/plot_info.py
+++ app/interfaces/plot_info.py
@@ -1,10 +1,10 @@
-# abstract class for basic plot information (shared between flora and fauna)
+# Abstract class access to basic plot information for both Flora and Faun
 
 from abc import ABC, abstractmethod
 
 class PlotInformation(ABC):
     @abstractmethod
-    def get_temp(self) -> float:
+    def get_current_temperature(self, day: int) -> float:
         pass
 
     @abstractmethod
@@ -13,4 +13,12 @@ class PlotInformation(ABC):
 
     @abstractmethod
     def get_all_flora(self) -> list:
+        pass
+
+    @abstractmethod
+    def get_avg_snow_height(self) -> float:
+        pass
+
+    @abstractmethod
+    def get_previous_snow_height(self) -> float:
         pass
