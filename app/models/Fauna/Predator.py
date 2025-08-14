4e50091592025e0375a638781875b0c8cc817e80
Aladin <aaladino581@gmail.com>
2025-08-14 10:45:24 -0600

    Update population in _update_mass_from_growth.

--- app/models/Fauna/Predator.py
+++ app/models/Fauna/Predator.py
@@ -11,7 +11,7 @@ class Predator(Fauna):
 
     def __init__(self, name: str, description: str, population: int, avg_mass: float,
                  ideal_temp_range: Tuple[float, float], ideal_food_range: Tuple[float, float], ideal_growth_rate: float, 
-                 feeding_rate: float, avg_steps_taken: int, avg_feet_area: float, plot: PlotInformation, prey: List['Fauna']):
+                 feeding_rate: float, avg_steps_taken: float, avg_feet_area: float, plot: PlotInformation, prey: List['Fauna']):
         
         super().__init__(name, description, population, avg_mass, ideal_growth_rate, ideal_temp_range, 
                         ideal_food_range, feeding_rate, avg_steps_taken, avg_feet_area, plot)
@@ -127,6 +127,11 @@ class Predator(Fauna):
         actual_growth_rate = base_growth_rate
         new_mass = self.get_total_mass() + self.get_total_mass() * actual_growth_rate
         self.set_total_mass(max(0, new_mass))  # Prevent negative mass
+        
+        # Update population
+        if self.avg_mass > 0:
+            new_population = int(new_mass / self.avg_mass)
+            self.population = max(0, new_population)
 
     def capacity_penalty(self) -> None:
         """
